b1f4a0e160711aa49aaee3f7785411887b57fc2d
Sahil Jain <sjain.735@gmail.com>
2016-12-23 17:45:44 +0530

    23-21-2016  5:45 pm

--- app/src/main/java/com/example/sahiljain/game_tic_tac_toe/Game_Screen.java
+++ app/src/main/java/com/example/sahiljain/game_tic_tac_toe/Game_Screen.java
@@ -40,9 +40,20 @@ public class Game_Screen extends AppCompatActivity implements View.OnClickListen
         image_8 = (ImageView) findViewById(R.id.game_sign_8);
         image_9 = (ImageView) findViewById(R.id.game_sign_9);
 
+        player_icon = 111; comp_icon = 0;
+
+        image_1.setOnClickListener(this);
+        image_2.setOnClickListener(this);
+        image_3.setOnClickListener(this);
+        image_4.setOnClickListener(this);
+        image_5.setOnClickListener(this);
+        image_6.setOnClickListener(this);
+        image_7.setOnClickListener(this);
+        image_8.setOnClickListener(this);
+        image_9.setOnClickListener(this);
 
         text_level.setText("LEVEL : " + level);
-
+/*
         new AlertDialog.Builder(getApplicationContext())
                 .setTitle("Icon_Selection")
                 .setMessage("Choose an Icon:")
@@ -62,6 +73,7 @@ public class Game_Screen extends AppCompatActivity implements View.OnClickListen
                 })
                 .setIcon(android.R.drawable.ic_dialog_alert)
                 .show();
+*/
 
 //--------------------------------------------------------------------------------------------------
         if(level.equalsIgnoreCase("Easy")){
@@ -83,6 +95,89 @@ public class Game_Screen extends AppCompatActivity implements View.OnClickListen
 
     @Override
     public void onClick(View v) {
+
+        if (v.getId()==R.id.game_sign_1){
+            if (player_icon==111){
+                image_1.setImageResource(R.drawable.icon_cross);
+            }
+            if(player_icon==0){
+                image_1.setImageResource(R.drawable.icon_circle);
+            }
+        }//Image_1
+
+        if (v.getId()==R.id.game_sign_2){
+            if (player_icon==111){
+                image_2.setImageResource(R.drawable.icon_cross);
+            }
+            if(player_icon==0){
+                image_2.setImageResource(R.drawable.icon_circle);
+            }
+        }//Image_2
+
+        if (v.getId()==R.id.game_sign_3){
+            if (player_icon==111){
+                image_3.setImageResource(R.drawable.icon_cross);
+            }
+            if(player_icon==0){
+                image_3.setImageResource(R.drawable.icon_circle);
+            }
+        }//Image_3
+
+        if (v.getId()==R.id.game_sign_4){
+            if (player_icon==111){
+                image_4.setImageResource(R.drawable.icon_cross);
+            }
+            if(player_icon==0){
+                image_4.setImageResource(R.drawable.icon_circle);
+            }
+
+        }//Image_4
+        if (v.getId()==R.id.game_sign_5){
+            if (player_icon==111){
+                image_5.setImageResource(R.drawable.icon_cross);
+            }
+            if(player_icon==0){
+                image_5.setImageResource(R.drawable.icon_circle);
+            }
+
+        }//Image_5
+
+        if (v.getId()==R.id.game_sign_6){
+            if (player_icon==111){
+                image_6.setImageResource(R.drawable.icon_cross);
+            }
+            if(player_icon==0){
+                image_6.setImageResource(R.drawable.icon_circle);
+            }
+
+        }//Image_6
+
+        if (v.getId()==R.id.game_sign_7){
+            if (player_icon==111){
+                image_7.setImageResource(R.drawable.icon_cross);
+            }
+            if(player_icon==0){
+                image_7.setImageResource(R.drawable.icon_circle);
+            }
+        }//Image_7
+
+        if (v.getId()==R.id.game_sign_8){
+            if (player_icon==111){
+                image_8.setImageResource(R.drawable.icon_cross);
+            }
+            if(player_icon==0){
+                image_8.setImageResource(R.drawable.icon_circle);
+            }
+        }//Image_8
+
+        if (v.getId()==R.id.game_sign_9){
+            if (player_icon==111){
+                image_9.setImageResource(R.drawable.icon_cross);
+            }
+            if(player_icon==0){
+                image_9.setImageResource(R.drawable.icon_circle);
+            }
+        }//Image_9
 
     }//onClick
 
