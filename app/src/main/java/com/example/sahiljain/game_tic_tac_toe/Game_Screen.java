06c1f3a61b891c01bdccb240a48b7562266c17c6
Sahil Jain <sjain.735@gmail.com>
2016-12-29 17:50:43 +0530

    29-12-2016  5:50 pm

--- app/src/main/java/com/example/sahiljain/game_tic_tac_toe/Game_Screen.java
+++ app/src/main/java/com/example/sahiljain/game_tic_tac_toe/Game_Screen.java
@@ -1,8 +1,5 @@
 package com.example.sahiljain.game_tic_tac_toe;
 
-import android.app.Dialog;
-import android.content.DialogInterface;
-import android.content.Intent;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,9 +7,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.Switch;
 import android.widget.TextView;
 
 import java.util.Random;
@@ -62,28 +57,6 @@ public class Game_Screen extends AppCompatActivity implements View.OnClickListen
         image_9.setOnClickListener(this);
 
  //       text_level.setText("LEVEL : " + level);
-/*
-        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("Choose:");
-        builder.setPositiveButton("Cross", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                player_icon = 111; comp_icon = 0;
-                dialog.cancel();
-            }
-        }).create();
-
-        builder.setNegativeButton("Circle", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                player_icon = 0; comp_icon = 111;
-                finish();
-            }
-        }).create();
-
-        builder.setCancelable(false);
-        Dialog dialog = builder.show();
-*/
 
         LayoutInflater layoutInflater = LayoutInflater.from(this);
         View prompt_view = layoutInflater.inflate(R.layout.layout_alert_dialog,null);
@@ -115,162 +88,184 @@ public class Game_Screen extends AppCompatActivity implements View.OnClickListen
 
 
     public void comp_turn_easy (){ //A function to make computer's move in the game - Level Easy
-        Random r = new Random();
-        int i = r.nextInt(n);
+        if (n>0) {
+            Random r = new Random();
+            int i = r.nextInt(n);
 
-        if(comp_icon==0){
-            switch (moves[i]){
-                case 1 : image_1.setImageResource(R.drawable.icon_circle); break;
-                case 2 : image_2.setImageResource(R.drawable.icon_circle); break;
-                case 3 : image_3.setImageResource(R.drawable.icon_circle); break;
-                case 4 : image_4.setImageResource(R.drawable.icon_circle); break;
-                case 5 : image_5.setImageResource(R.drawable.icon_circle); break;
-                case 6 : image_6.setImageResource(R.drawable.icon_circle); break;
-                case 7 : image_7.setImageResource(R.drawable.icon_circle); break;
-                case 8 : image_8.setImageResource(R.drawable.icon_circle); break;
-                case 9 : image_9.setImageResource(R.drawable.icon_circle); break;
-            }//switch - circle
-        }//if
-        else if (comp_icon == 111){
-            switch (moves[i]){
-                case 1 : image_1.setImageResource(R.drawable.icon_cross); break;
-                case 2 : image_2.setImageResource(R.drawable.icon_cross); break;
-                case 3 : image_3.setImageResource(R.drawable.icon_cross); break;
-                case 4 : image_4.setImageResource(R.drawable.icon_cross); break;
-                case 5 : image_5.setImageResource(R.drawable.icon_cross); break;
-                case 6 : image_6.setImageResource(R.drawable.icon_cross); break;
-                case 7 : image_7.setImageResource(R.drawable.icon_cross); break;
-                case 8 : image_8.setImageResource(R.drawable.icon_cross); break;
-                case 9 : image_9.setImageResource(R.drawable.icon_cross); break;
-            }//switch - cross
-        }//else if
+            if(comp_icon==0){
+                switch (moves[i]){
+                    case 1 : image_1.setImageResource(R.drawable.icon_circle); break;
+                    case 2 : image_2.setImageResource(R.drawable.icon_circle); break;
+                    case 3 : image_3.setImageResource(R.drawable.icon_circle); break;
+                    case 4 : image_4.setImageResource(R.drawable.icon_circle); break;
+                    case 5 : image_5.setImageResource(R.drawable.icon_circle); break;
+                    case 6 : image_6.setImageResource(R.drawable.icon_circle); break;
+                    case 7 : image_7.setImageResource(R.drawable.icon_circle); break;
+                    case 8 : image_8.setImageResource(R.drawable.icon_circle); break;
+                    case 9 : image_9.setImageResource(R.drawable.icon_circle); break;
+                }//switch - circle
+            }//if
+            else if (comp_icon == 111){
+                switch (moves[i]){
+                    case 1 : image_1.setImageResource(R.drawable.icon_cross); break;
+                    case 2 : image_2.setImageResource(R.drawable.icon_cross); break;
+                    case 3 : image_3.setImageResource(R.drawable.icon_cross); break;
+                    case 4 : image_4.setImageResource(R.drawable.icon_cross); break;
+                    case 5 : image_5.setImageResource(R.drawable.icon_cross); break;
+                    case 6 : image_6.setImageResource(R.drawable.icon_cross); break;
+                    case 7 : image_7.setImageResource(R.drawable.icon_cross); break;
+                    case 8 : image_8.setImageResource(R.drawable.icon_cross); break;
+                    case 9 : image_9.setImageResource(R.drawable.icon_cross); break;
+                }//switch - cross
+            }//else if
 
+            array_deletion(moves,moves[i]);
+
+        }//if n>0
     }//comp_turn_easy
 
-    public void array_deletion (int [] a, int pos){ //Deletion Fn
+    public void array_deletion (int [] a, int num){ //Deletion Fn
+       if (n>0) {
+           Log.d("Array", String.valueOf(a[0]));
+           Log.d("Array", String.valueOf(a[1]));
+           Log.d("Array", String.valueOf(a[2]));
+           Log.d("Array", String.valueOf(a[3]));
+           Log.d("Array", String.valueOf(a[4]));
+           Log.d("Array", String.valueOf(a[5]));
+           Log.d("Array", String.valueOf(a[6]));
+           Log.d("Array", String.valueOf(a[7]));
+           Log.d("Array", String.valueOf(a[8]));
+           Log.d("N", String.valueOf(n));
 
-        for (int i=pos-1;i<n-1;i++){
-            a[i]=a[i+1];
-        }//for
-        n--;
+           int i, pos = 0;
 
+           for (i = 0; i < n; i++) {
+               if (a[i] == num) {
+                   pos = i;
+                   break;
+               }
+           }
+
+           Log.d("Pos", String.valueOf(pos));
+
+           for (i = pos; i < n - 1; i++) {
+               a[i] = a[i + 1];
+           }//for
+           n--;
+
+           Log.d("Array", String.valueOf(a[0]));
+           Log.d("Array", String.valueOf(a[1]));
+           Log.d("Array", String.valueOf(a[2]));
+           Log.d("Array", String.valueOf(a[3]));
+           Log.d("Array", String.valueOf(a[4]));
+           Log.d("Array", String.valueOf(a[5]));
+           Log.d("Array", String.valueOf(a[6]));
+           Log.d("Array", String.valueOf(a[7]));
+           Log.d("Array", String.valueOf(a[8]));
+           Log.d("N", String.valueOf(n));
+       }//if n>0
     }//del function
 
 
     @Override
     public void onClick(View v) {
-/*
+
         if (player_icon == 111){
             switch (v.getId()){
-                case R.id.game_sign_1 : image_1.setImageResource(R.drawable.icon_cross); break;
-                case R.id.game_sign_2 : image_2.setImageResource(R.drawable.icon_cross); break;
-                case R.id.game_sign_3 : image_3.setImageResource(R.drawable.icon_cross); break;
-                case R.id.game_sign_4 : image_4.setImageResource(R.drawable.icon_cross); break;
-                case R.id.game_sign_5 : image_5.setImageResource(R.drawable.icon_cross); break;
-                case R.id.game_sign_6 : image_6.setImageResource(R.drawable.icon_cross); break;
-                case R.id.game_sign_7 : image_7.setImageResource(R.drawable.icon_cross); break;
-                case R.id.game_sign_8 : image_8.setImageResource(R.drawable.icon_cross); break;
-                case R.id.game_sign_9 : image_9.setImageResource(R.drawable.icon_cross); break;
+                case R.id.game_sign_1 : image_1.setImageResource(R.drawable.icon_cross);
+                                        array_deletion(moves,1);
+                                        comp_turn_easy();
+                                        break;
+
+                case R.id.game_sign_2 : image_2.setImageResource(R.drawable.icon_cross);
+                                        array_deletion(moves,2);
+                                        comp_turn_easy();
+                                        break;
+
+                case R.id.game_sign_3 : image_3.setImageResource(R.drawable.icon_cross);
+                                        array_deletion(moves,3);
+                                        comp_turn_easy();
+                                        break;
+
+                case R.id.game_sign_4 : image_4.setImageResource(R.drawable.icon_cross);
+                                        array_deletion(moves,4);
+                                        comp_turn_easy();
+                                        break;
+
+                case R.id.game_sign_5 : image_5.setImageResource(R.drawable.icon_cross);
+                                        array_deletion(moves,5);
+                                        comp_turn_easy();
+                                        break;
+
+                case R.id.game_sign_6 : image_6.setImageResource(R.drawable.icon_cross);
+                                        array_deletion(moves,6);
+                                        comp_turn_easy();
+                                        break;
+
+                case R.id.game_sign_7 : image_7.setImageResource(R.drawable.icon_cross);
+                                        array_deletion(moves,7);
+                                        comp_turn_easy();
+                                        break;
+
+                case R.id.game_sign_8 : image_8.setImageResource(R.drawable.icon_cross);
+                                        array_deletion(moves,8);
+                                        comp_turn_easy();
+                                        break;
+
+                case R.id.game_sign_9 : image_9.setImageResource(R.drawable.icon_cross);
+                                        array_deletion(moves,9);
+                                        comp_turn_easy();
+                                        break;
             }//switch - cross
         }//if
         else if (player_icon == 0){
             switch (v.getId()){
-                case R.id.game_sign_1 : image_1.setImageResource(R.drawable.icon_circle); break;
-                case R.id.game_sign_2 : image_2.setImageResource(R.drawable.icon_circle); break;
-                case R.id.game_sign_3 : image_3.setImageResource(R.drawable.icon_circle); break;
-                case R.id.game_sign_4 : image_4.setImageResource(R.drawable.icon_circle); break;
-                case R.id.game_sign_5 : image_5.setImageResource(R.drawable.icon_circle); break;
-                case R.id.game_sign_6 : image_6.setImageResource(R.drawable.icon_circle); break;
-                case R.id.game_sign_7 : image_7.setImageResource(R.drawable.icon_circle); break;
-                case R.id.game_sign_8 : image_8.setImageResource(R.drawable.icon_circle); break;
-                case R.id.game_sign_9 : image_9.setImageResource(R.drawable.icon_circle); break;
+                case R.id.game_sign_1 : image_1.setImageResource(R.drawable.icon_circle);
+                    array_deletion(moves,1);
+                    comp_turn_easy();
+                    break;
+
+                case R.id.game_sign_2 : image_2.setImageResource(R.drawable.icon_circle);
+                    array_deletion(moves,2);
+                    comp_turn_easy();
+                    break;
+
+                case R.id.game_sign_3 : image_3.setImageResource(R.drawable.icon_circle);
+                    array_deletion(moves,3);
+                    comp_turn_easy();
+                    break;
+
+                case R.id.game_sign_4 : image_4.setImageResource(R.drawable.icon_circle);
+                    array_deletion(moves,4);
+                    comp_turn_easy();
+                    break;
+
+                case R.id.game_sign_5 : image_5.setImageResource(R.drawable.icon_circle);
+                    array_deletion(moves,5);
+                    comp_turn_easy();
+                    break;
+
+                case R.id.game_sign_6 : image_6.setImageResource(R.drawable.icon_circle);
+                    array_deletion(moves,6);
+                    comp_turn_easy();
+                    break;
+
+                case R.id.game_sign_7 : image_7.setImageResource(R.drawable.icon_circle);
+                    array_deletion(moves,7);
+                    comp_turn_easy();
+                    break;
+
+                case R.id.game_sign_8 : image_8.setImageResource(R.drawable.icon_circle);
+                    array_deletion(moves,8);
+                    comp_turn_easy();
+                    break;
+
+                case R.id.game_sign_9 : image_9.setImageResource(R.drawable.icon_circle);
+                    array_deletion(moves,9);
+                    comp_turn_easy();
+                    break;
             }//switch - circle
         }//else if
-*/
-
-        if (v.getId()==R.id.game_sign_1){
-            if (player_icon==111){
-                image_1.setImageResource(R.drawable.icon_cross);
-            }
-            else if(player_icon==0){
-                image_1.setImageResource(R.drawable.icon_circle);
-            }
-        }//Image_1
-
-        if (v.getId()==R.id.game_sign_2){
-            if (player_icon==111){
-                image_2.setImageResource(R.drawable.icon_cross);
-            }
-            else if(player_icon==0){
-                image_2.setImageResource(R.drawable.icon_circle);
-            }
-        }//Image_2
-
-        if (v.getId()==R.id.game_sign_3){
-            if (player_icon==111){
-                image_3.setImageResource(R.drawable.icon_cross);
-            }
-            else if(player_icon==0){
-                image_3.setImageResource(R.drawable.icon_circle);
-            }
-        }//Image_3
-
-        if (v.getId()==R.id.game_sign_4){
-            if (player_icon==111){
-                image_4.setImageResource(R.drawable.icon_cross);
-            }
-            else if(player_icon==0){
-                image_4.setImageResource(R.drawable.icon_circle);
-            }
-
-        }//Image_4
-        if (v.getId()==R.id.game_sign_5){
-            if (player_icon==111){
-                image_5.setImageResource(R.drawable.icon_cross);
-            }
-            else if(player_icon==0){
-                image_5.setImageResource(R.drawable.icon_circle);
-            }
-
-        }//Image_5
-
-        if (v.getId()==R.id.game_sign_6){
-            if (player_icon==111){
-                image_6.setImageResource(R.drawable.icon_cross);
-            }
-            else if(player_icon==0){
-                image_6.setImageResource(R.drawable.icon_circle);
-            }
-
-        }//Image_6
-
-        if (v.getId()==R.id.game_sign_7){
-            if (player_icon==111){
-                image_7.setImageResource(R.drawable.icon_cross);
-            }
-            else if(player_icon==0){
-                image_7.setImageResource(R.drawable.icon_circle);
-            }
-        }//Image_7
-
-        if (v.getId()==R.id.game_sign_8){
-            if (player_icon==111){
-                image_8.setImageResource(R.drawable.icon_cross);
-            }
-            else if(player_icon==0){
-                image_8.setImageResource(R.drawable.icon_circle);
-            }
-        }//Image_8
-
-        if (v.getId()==R.id.game_sign_9){
-            if (player_icon==111){
-                image_9.setImageResource(R.drawable.icon_cross);
-            }
-            else if(player_icon==0){
-                image_9.setImageResource(R.drawable.icon_circle);
-            }
-        }//Image_9
-
     }//onClick
 
 }//Class Game Screen
